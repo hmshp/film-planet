89980818a952bca3b0a2b6a000fca336bc4a5cca
hmshp <hyemisonne@gmail.com>
2022-11-23 17:40:14 +0900

    사용하지 않는 파라미터 삭제

--- client/src/utils/token.js
+++ client/src/utils/token.js
@@ -4,10 +4,10 @@ export const saveToken = (token) => {
   localStorage.setItem(TOKEN, token);
 };
 
-export const getToken = (token) => {
+export const getToken = () => {
   return localStorage.getItem(TOKEN);
 };
 
-export const deleteToken = (token) => {
+export const deleteToken = () => {
   return localStorage.clear(TOKEN);
 };
