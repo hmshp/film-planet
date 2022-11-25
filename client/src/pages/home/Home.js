4fa82918cdc9d06553b41abd443e56e69f0d4e29
hmshp <hyemisonne@gmail.com>
2022-11-25 19:21:11 +0900

    토큰 redux에 저장 후 로그인 여부 파악 -> 컴포넌트 조건부 렌더링

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
+import MyHome from '../../components/MyHome';
+import Welcome from '../../components/Welcome';
+import { userSlice } from '../../redux/authSlice';
+import { asyncGetPosts } from '../../redux/postSlice';
+import { getPosts } from '../../service/posts';
+import { getToken } from '../../utils/token';
 
 const tempBorderStyle = {
   border: "3px solid pink",
@@ -7,15 +14,26 @@ const tempBorderStyle = {
 };
 
 const Home = () => {
+  const dispatch = useDispatch();
+  
+  // redux에 토큰 있는지 확인 (= 로그인했는지)
+  // 로그인했다면 localStorage에 토큰이 저장되어 있을 테고, 그러면 useEffect에서 그 토큰을 가져와 redux에 저장했을 테니까
+  const user = useSelector((state) => {
+    return state.user;
+  })
+  const isLoggedIn = () => {
+    return user.token? true : false;
+  }
+
+  const token = getToken();
+  useEffect(() => {
+    // localStorage에 있는 토큰 redux에 저장
+    dispatch(userSlice.actions.getToken(token));
+  }, []);
+
+
   return (
-    <main style={tempBorderStyle}>
-      <article>
-        <h1>나만의 영화 저장소를 만들어 보세요.</h1>
-        <Link to="/signup">
-          <button>지금 시작하기</button>
-        </Link>
-      </article>
-    </main>
+    isLoggedIn() ? <MyHome /> : <Welcome />
   );
 };
 
