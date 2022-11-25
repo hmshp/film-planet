a9a94176ba84c0f9ec41e392388ee8ad487aae18
hmshp <hyemisonne@gmail.com>
2022-11-25 19:29:16 +0900

    회원가입 액션 dispatch

--- client/src/pages/user/Signup.js
+++ client/src/pages/user/Signup.js
@@ -1,36 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from "react-router-dom";
+import { asyncSignup } from '../../redux/authSlice';
 
 const Signup = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    name: "",
+    email: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData((prevData) => {
+      return { ...prevData, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(asyncSignup(formData))
+  };
 
   return (
     <>
       <main>
         <h1>회원가입</h1>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            navigate("/");
-          }}
-        >
+        <form>
           <div>
             <label htmlFor="username">아이디: </label>
-            <input type="text" name="username" id="username" />
+            <input
+              onChange={handleChange}
+              type="text"
+              name="username"
+              id="username"
+            />
           </div>
           <div>
             <label htmlFor="password">비밀번호:</label>
-            <input type="password" name="password" id="password" />
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              id="password"
+            />
           </div>
           <div>
             <label htmlFor="name">이름:</label>
-            <input type="text" name="name" id="name" />
+            <input onChange={handleChange} type="text" name="name" id="name" />
           </div>
           <div>
             <label htmlFor="email">이메일:</label>
-            <input type="email" name="email" id="email" />
+            <input
+              onChange={handleChange}
+              type="email"
+              name="email"
+              id="email"
+            />
           </div>
-          <button>회원가입</button>
+          <button onClick={handleSubmit}>회원가입</button>
         </form>
       </main>
       <aside>
