38730fae77fd38749a86697e2004ef755a5a3b37
hmshp <hyemisonne@gmail.com>
2022-11-25 19:28:16 +0900

    postSlice 추가

--- client/src/redux/configStore.js
+++ client/src/redux/configStore.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "./authSlice.js";
+import { postSlice } from './postSlice.js';
+
 
 const store = configureStore({
   reducer: {
-    user: userSlice.reducer
+    user: userSlice.reducer,
+    post: postSlice.reducer
   },
 });
 
