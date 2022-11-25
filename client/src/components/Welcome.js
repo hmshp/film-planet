ab28349bf8f3e8f139259bfaa194eaa57fd52909
hmshp <hyemisonne@gmail.com>
2022-11-25 19:21:27 +0900

    비회원에게 보이는 Welcome 컴포넌트 생성

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const tempBorderStyle = {
+  border: "3px solid pink",
+  padding: "24px",
+};
+
+const Welcome = () => {
+
+  return (
+    <main style={tempBorderStyle}>
+      <article>
+        <h1>나만의 영상 저장소를 만들어 보세요.</h1>
+        <Link to="/signup">
+          <button>지금 시작하기</button>
+        </Link>
+      </article>
+    </main>
+  );
+};
+
+export default Welcome;
