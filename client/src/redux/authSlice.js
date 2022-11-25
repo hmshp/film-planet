32ad6576c769b23f416e16bec4615e21827ca3bc
hmshp <hyemisonne@gmail.com>
2022-11-25 19:18:35 +0900

    회원가입 로직 추가

--- client/src/redux/authSlice.js
+++ client/src/redux/authSlice.js
@@ -1,7 +1,6 @@
-import React from "react";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import login from "../service/auth";
-import { saveToken } from "../utils/token";
+import { login, signup } from "../service/auth";
+import { getToken, saveToken } from "../utils/token";
 
 export const asyncLogin = createAsyncThunk(
   "authSlice/asyncLogin",
@@ -11,14 +10,22 @@ export const asyncLogin = createAsyncThunk(
   }
 );
 
+export const asyncSignup = createAsyncThunk(
+  "authSlice/asyncSignup",
+  async (user) => {
+    const response = await signup(user);
+    console.log(response);
+  }  
+)
+
 export const userSlice = createSlice({
   name: "user",
-  initialState: { username: "", token: "", status: "Welcome" },
-  // reducers: {
-  //   setUsername: (state, action) => {
-  //     state.username = action.username;
-  //   },
-  // },
+  initialState: { token: "", status: "Welcome" },
+  reducers: {
+    getToken: (state, action) => {
+      state.token = action.payload;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(asyncLogin.pending, (state, action) => {
       state.status = "Loading";
@@ -26,11 +33,9 @@ export const userSlice = createSlice({
     builder.addCase(asyncLogin.fulfilled, (state, action) => {
       state.status = "Complete";
       saveToken(action.payload.token);
-      state.username = action.payload.username;
-      state.token = action.payload.token;
     });
     builder.addCase(asyncLogin.rejected, (state, action) => {
-      state.status = "Fail";
+      state.status = "Failed to load";
     });
   },
 });
