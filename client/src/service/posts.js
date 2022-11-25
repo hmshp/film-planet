462874b07d36824466257aa4d2383b6c124a3171
hmshp <hyemisonne@gmail.com>
2022-11-25 19:33:08 +0900

    게시물 추가, id로 게시물 조회하기 API 호출

--- client/src/service/posts.js
+++ client/src/service/posts.js
@@ -1,20 +1,42 @@
 import axios from "axios";
-import { getToken } from '../utils/token';
+import { getToken } from "../utils/token";
 
 const baseUrl = "http://localhost:8080";
 
 // 요청 보낼 때 Header Authorization으로 jwt 토큰을 보내면 서버에서 userId를 알아서 찾은 다음 그걸 활용해서 클라이언트의 요청을 처리해 준다
 export async function getPosts() {
   const posts = await axios.get(`${baseUrl}/posts`, {
-    headers: getHeaders()
+    headers: getHeaders(),
+  });
+  return posts;
+}
+
+export async function createPost(post) {
+  console.log(post);
+  const response = await axios.post(`${baseUrl}/posts`,
+    {
+      title: post.title,
+      review: post.review,
+      url: post.url,
+      comment: post.comment
+    },
+    {
+      headers: getHeaders(),
+    }
+  );
+  return response;
+}
+
+export async function getPostById(id) {
+  const response = await axios.get(`${baseUrl}/posts/${id}`, {
+    headers: getHeaders(),
   })
-  console.log(posts);
+  return response;
 }
 
 function getHeaders() {
   const token = getToken();
-  console.log(token);
-  return { Authorization: `Bearer ${token}`}
+  return { Authorization: `Bearer ${token}` };
 }
 
 // createPost 요청 보낼 때 파라미터로 userId도 보내야 되는데
@@ -27,4 +49,3 @@ function getHeaders() {
 // }
 
 // 서버에 jwt token 보내주면서 "나야..!" 인증하고 userId 받기
-
