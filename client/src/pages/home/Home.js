cf31acd555975833c3af4562023b1753d2f02875
hmshp <hyemisonne@gmail.com>
2022-12-01 19:46:14 +0900

    feat: username 존재 여부에 따라 조건부 렌더링 (me API 호출 후 정상 응답 시 username 리턴하고, 401 에러 발생 시 null 리턴한다는 점을 이용)

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,40 +1,19 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from "react-router-dom";
-import MyHome from '../../components/MyHome';
-import Welcome from '../../components/Welcome';
-import { userSlice } from '../../redux/authSlice';
-import { asyncGetPosts } from '../../redux/postSlice';
-import { getPosts } from '../../service/posts';
-import { getToken } from '../../utils/token';
-
-const tempBorderStyle = {
-  border: "3px solid pink",
-  padding: "24px",
-};
+import { useDispatch, useSelector } from "react-redux";
+import MyHome from "../../components/MyHome";
+import Welcome from "../../components/Welcome";
+import { asyncCheckValidLogin } from "../../redux/authSlice";
 
 const Home = () => {
   const dispatch = useDispatch();
-  
-  // redux에 토큰 있는지 확인 (= 로그인했는지)
-  // 로그인했다면 localStorage에 토큰이 저장되어 있을 테고, 그러면 useEffect에서 그 토큰을 가져와 redux에 저장했을 테니까
-  const user = useSelector((state) => {
-    return state.user;
-  })
-  const isLoggedIn = () => {
-    return user.token? true : false;
-  }
+  const username = useSelector((state) => state.user.username)
 
-  const token = getToken();
   useEffect(() => {
-    // localStorage에 있는 토큰 redux에 저장
-    dispatch(userSlice.actions.getToken(token));
-  }, []);
+    // 로그인 되어 있는지 & 유효한 사용자인지(토큰 만료되지 않았는지) 체크
+    dispatch(asyncCheckValidLogin());
+  }, [username]);
 
-
-  return (
-    isLoggedIn() ? <MyHome /> : <Welcome />
-  );
+  return username ? <MyHome /> : <Welcome />;
 };
 
 export default Home;
