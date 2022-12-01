5b1efd90b24ca90670dcb935bba1abc16e55ff26
hmshp <hyemisonne@gmail.com>
2022-12-01 20:11:41 +0900

    fix: 로그인 전 & 로그아웃 후에도 401 에러 콘솔에 찍히는 문제 해결 (토큰이 없을 때는 me API 요청을 아예 안함으로써 401에러 발생 방지)

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -4,19 +4,21 @@ import MyHome from "../../components/MyHome";
 import Welcome from "../../components/Welcome";
 import { asyncCheckValidLogin } from "../../redux/authSlice";
 import { isValidLogin } from "../../utils/loginCheck";
+import { getToken } from "../../utils/token";
 
 const Home = () => {
   const dispatch = useDispatch();
   const username = useSelector((state) => state.user.username);
+  const token = getToken();
 
   useEffect(() => {
     // 로그인 되어 있는지 & 유효한 사용자인지(토큰 만료되지 않았는지) 체크
-    dispatch(asyncCheckValidLogin());
+    token && dispatch(asyncCheckValidLogin());
   }, [username]);
 
-  console.log(isValidLogin(username));
-
-  return isValidLogin(username) ? <MyHome /> : <Welcome />;
+  return !token ? (
+    <Welcome />
+  ) : isValidLogin(username) ? <MyHome /> : <Welcome />;
 };
 
 export default Home;
