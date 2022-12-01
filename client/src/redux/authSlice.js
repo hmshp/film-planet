0a5e80ffa10d65715edfae0f14b78836bfb5965f
hmshp <hyemisonne@gmail.com>
2022-12-01 19:33:46 +0900

    feat: me API 호출 후 redux에 username 저장

--- client/src/redux/authSlice.js
+++ client/src/redux/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { login, signup } from "../service/auth";
+import { login, me, signup } from "../service/auth";
 import { getToken, saveToken } from "../utils/token";
 
 export const asyncLogin = createAsyncThunk(
@@ -15,17 +15,25 @@ export const asyncSignup = createAsyncThunk(
   async (user) => {
     const response = await signup(user);
     console.log(response);
-  }  
-)
+  }
+);
+
+export const asyncCheckValidLogin = createAsyncThunk(
+  "authSlice/asyncCheckValidLogin",
+  async () => {
+    const response = await me();
+    return response;
+  }
+);
 
 export const userSlice = createSlice({
   name: "user",
-  initialState: { token: "", status: "Welcome" },
-  reducers: {
-    getToken: (state, action) => {
-      state.token = action.payload;
-    },
-  },
+  initialState: { username: "", status: "Welcome" },
+  // reducers: {
+  //   getToken: (state, action) => {
+  //     state.token = action.payload;
+  //   },
+  // },
   extraReducers: (builder) => {
     builder.addCase(asyncLogin.pending, (state, action) => {
       state.status = "Loading";
@@ -36,6 +44,10 @@ export const userSlice = createSlice({
     });
     builder.addCase(asyncLogin.rejected, (state, action) => {
       state.status = "Failed to load";
+    });
+    builder.addCase(asyncCheckValidLogin.fulfilled, (state, action) => {
+      console.log(action.payload);
+      state.username = action.payload;
     });
   },
 });
