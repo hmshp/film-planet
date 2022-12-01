3ca0ea568d8c18e7dbdf9c045cf381786ad7e3c2
hmshp <hyemisonne@gmail.com>
2022-12-01 17:27:43 +0900

    feat: auth 라우터 설정

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,10 +1,12 @@
 import express from "express";
 import "express-async-errors";
 import * as authController from "../controller/auth.js";
+import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.get("/me", isAuth, authController.me);
 
 export default router;
