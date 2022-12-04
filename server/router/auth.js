7d5447ee4683fbae63228582d2637e791e27887e
hmshp <hyemisonne@gmail.com>
2022-12-04 12:34:22 +0900

    feat: 로그인, 회원가입 유효성 검사

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,12 +1,33 @@
 import express from "express";
 import "express-async-errors";
+import { body } from "express-validator";
 import * as authController from "../controller/auth.js";
-import { isAuth } from '../middleware/auth.js';
+import { isAuth } from "../middleware/auth.js";
+import { validate } from "../middleware/validator.js";
 
 const router = express.Router();
 
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
+const validateCredential = [
+  body("username")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("아이디는 4글자 이상이어야 합니다"),
+  body("password")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("비밀번호는 4글자 이상이어야 합니다"),
+  validate,
+];
+
+const validateSignup = [
+  ...validateCredential,
+  body('name').notEmpty().withMessage('이름을 입력해 주세요.'),
+  body('email').isEmail().normalizeEmail().withMessage('유효하지 않은 이메일입니다.'),
+  validate,
+];
+
+router.post("/signup", validateSignup, authController.signup);
+router.post("/login", validateCredential, authController.login);
 router.get("/me", isAuth, authController.me);
 
 export default router;
