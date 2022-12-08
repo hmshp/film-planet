5773df0a9b815e4ac09f56f70afa7213e6a3c03f
hmshp <hyemisonne@gmail.com>
2022-12-08 16:53:27 +0900

    style: GlobalStyles 사용

---
+++ client/src/styles/GlobalVariables.js
@@ -0,0 +1,12 @@
+import { css } from 'styled-components';
+
+export const GlobalVariables = css`
+  :root {
+    --primary-color-dark: #1b2831;
+    --primary-color-light: #ffffff;
+    --primary-color-gradient: linear-gradient(202.17deg,#1400ff 8.58%,#ad00ff 91.42%);
+    --primary-color-gradient-secondary: linear-gradient(202.17deg,#1400ffc4 8.58%,#ad00ffcf 91.42%);
+    --secondary-color: #c89efc;
+    --font-family: 'Inter', sans-serif;
+  }
+`;
